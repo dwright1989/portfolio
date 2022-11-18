d70724b6f046b1a45457bc47f9a35085a7f09b09
dwright1989 <debbiewright@live.co.uk>
2022-11-18 14:50:38 +0000

    Contact section updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,3 +1,7 @@
+import githubIcon from '../Images/github.jpg';
+import linkedinIcon from '../Images/linkedin.png';
+import emailIcon from '../Images/email.png';
+
 
 function Contact() {
 
@@ -6,8 +10,10 @@ function Contact() {
           <div id="contact">
               <h2>Contact Me</h2>
               <p>If you wish to contact me, please reach out on either my LinkedIn or Email provided below.</p> 
-              <p>debbiewright@live.co.uk</p>
-
+              <p><img id="emailIcon" src={emailIcon} alt="email" className="social-icon"/><span id="email">debbiewright@live.co.uk</span></p>
+              <p><a href="https://github.com/dwright1989" target="_blank"><img src={githubIcon} alt="github" className="social-icon"/></a>
+              <a href="" target="_blank"><img src={linkedinIcon} alt="linkedin" className="social-icon"/></a></p>
+            
           </div>
     </div> 
   );
