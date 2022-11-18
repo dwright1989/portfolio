f2b3c9b2e0c720fe49a885601490dd185ee3caec
dwright1989 <debbiewright@live.co.uk>
2022-11-18 12:49:18 +0000

    CSS for about section updated.  Still to update text and image

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,8 +5,13 @@ function AboutMe() {
 
     return (
     <div id="aboutMe">
+       <div className="draw">
+            <div className="rectangle"></div>
+        </div>
       <h2>Debbie Wright</h2>
+      <div id="imgDiv">
         <img src={profilePic} alt="profile"/>
+      </div>
         <div id="description">
           <h3>About Me</h3>
           <p>This is to be filled. This is to be filled.  This is to be filled.  
