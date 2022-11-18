7950b5d8bc4fa873aa37a7b431c471641f8aca2f
dwright1989 <debbiewright@live.co.uk>
2022-11-18 13:55:03 +0000

    CSS for project

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,9 +2,17 @@
 
 function Project(props) {
 
+    const proj = props.proj;
+
     return (
     <div className="project">
-        {props.proj.name}
+        <img src={proj.url} alt="project"/>
+        <div className="links">
+            <p>{proj.name}</p>
+            <a href="">Live</a>
+            <a href="">Repo</a>
+        </div>
+        <p>{proj.description}</p>
     </div>
   );
 }
