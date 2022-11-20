7544fcad7453613a505849c70c5609999da237f6
dwright1989 <debbiewright@live.co.uk>
2022-11-20 10:09:09 +0000

    My work section css updated for desktop

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -9,7 +9,7 @@ function Project(props) {
     <div className="project">
         <img src={proj.url} alt="project"/>
         <div className="links">
-            <p>{proj.name}</p>
+            <p className="proj-title">{proj.name}</p>
             <a href={proj.repo} target="_blank"><img src={githubIcon} alt="repo" className="social-icon"/></a>
             <a href={proj.live} target="_blank"><img src={openIcon} alt="live" className="social-icon"/></a>
         </div>
